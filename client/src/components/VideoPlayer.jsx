cdd7b46503ffd74478ba43ddc569dd17f30e4e87
Edmond GAKS <edmondgaks@gmail.com>
2022-07-22 10:56:18 +0200

    adding some styles and using them

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,18 +1,19 @@
 import React, { useContext } from 'react'
 import { Grid,Typography,Paper} from '@mui/material';
 // import { makeStyles } from '@material-ui/styles';
+import './style.css';
 
 import { SocketContext } from '../SocketContext';
 const VideoPlayer = () => {
   const {name,callAccepted,myVideo,userVideo,callEnded,stream,call} = useContext(SocketContext);
   return (
-    <Grid container>
+    <Grid container className="gridContainer">
       {
         stream && (
-          <Paper>
+          <Paper className="paper">
             <Grid item xs={12} md={6}>
               <Typography variant='h2' gutterBottom>{name || 'name'}</Typography>
-              <video playsInline muted ref={myVideo} autoPlay />
+              <video playsInline muted ref={myVideo} autoPlay className="video" />
             </Grid>
           </Paper>
         )
@@ -27,7 +28,6 @@ const VideoPlayer = () => {
           </Paper>
         )
       }
-          
     </Grid>
   )
 }
