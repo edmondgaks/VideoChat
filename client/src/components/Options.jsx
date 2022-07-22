22308447536efacba528446402d20cb46df45faa
Edmond GAKS <edmondgaks@gmail.com>
2022-07-22 11:47:17 +0200

    putting all 2 buttons and there functionalities

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -22,11 +22,13 @@ const Options = ({children}) => {
               </CopyToClipboard>
             </Grid>
             <Grid item xs={12} md={6} className="padding">
-              <Typography variant="h6" gutterBottom>Account Info</Typography>
-              <TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} fullWidth />
-              <CopyToClipboard text={me} className="margin">
-                <Button variant="contained" color="primary" fullWidth startIcon={<Assignment fontSize="large" />}>Copy your Id</Button>
-              </CopyToClipboard>
+              <Typography variant="h6" gutterBottom>Make a call</Typography>
+              <TextField label="Name" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} fullWidth />
+              {callAccepted && !callEnded ? (
+                <Button variant="contained" color="secondary" startIcon={<PhoneDisabled fontSize="large" />} fullWidth onClick={leaveCall} className="margin">Hang up</Button>
+              ) : (
+                <Button variant="contained" color="primary" startIcon={<Phone fontSize="large" />} fullWidth onClick={() => callUser(idToCall)} className="margin"></Button>
+              )}
             </Grid>
           </Grid>
         </form>
