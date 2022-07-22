e8f181e305234f8dc1e7f8f758fde81d53136156
Edmond GAKS <edmondgaks@gmail.com>
2022-07-22 11:59:45 +0200

    adding the button to answer a call

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -4,7 +4,14 @@ import { SocketContext } from '../SocketContext';
 const Notifications = () => {
   const { answerCall,call,callAccepted } = useContext(SocketContext);
   return (
-    <div></div>
+    <>
+      {call.isReceivedCall && !callAccepted && (
+        <div style={{display: 'flex', justifyContext: 'center'}}>
+          <h1>{call.name} is calling:</h1>
+          <Button variant="contained" color="primary" onClick={answerCall}>Answer</Button>
+        </div>
+      )}
+    </>
   )
 }
 
